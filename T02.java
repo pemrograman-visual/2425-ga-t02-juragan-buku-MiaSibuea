82c430330ffdd31aeb7678a71330432bc5ca13ab
MiaSibuea <miasibuea152@gmail.com>
2024-09-27 14:06:52 +0700

    pair 14

--- T02.java
+++ T02.java
@@ -1,1 +1,52 @@
-// NIM - Your Name
+// 12S24005-MIA NATHANIA SIBUEA
+// 12S24036-JOSEP CHRISTIAN MARPAUNG
+import java.util.*;
+import java.lang.Math;
+
+public class T02 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String iSBN, judul, penulis, penerbit, formatBuku, rate;
+        int tahunTerbit, stokBuku;
+        double hargaPembelian, minMargin, rating;
+
+        iSBN = input.nextLine();
+        judul = input.nextLine();
+        penulis = input.nextLine();
+        tahunTerbit = Integer.parseInt(input.nextLine());
+        penerbit = input.nextLine();
+        formatBuku = input.nextLine();
+        hargaPembelian = Double.parseDouble(input.nextLine());
+        minMargin = Double.parseDouble(input.nextLine());
+        stokBuku = Integer.parseInt(input.nextLine());
+        rating = Double.parseDouble(input.nextLine());
+        if (rating >= 4.7 && rating <= 5) {
+            rate = "Best Pick";
+        } else {
+            if (rating >= 4.5) {
+                rate = "Must Read";
+            } else {
+                if (rating >= 4.0) {
+                    rate = "Recommended";
+                } else {
+                    if (rating >= 3.0) {
+                        rate = "Average";
+                    } else {
+                        if (rating < 3) {
+                            rate = "Low";
+                        } else {
+                            rate = "";
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(iSBN + "|" + judul + "|" + penulis + "|" + tahunTerbit + "|" + penerbit + "|" + formatBuku + "|" + toFixed(hargaPembelian,1) + "|" + toFixed(minMargin,1) + "|" + stokBuku + "|" + toFixed(rating,1) + "|" + rate);
+    }
+    
+    private static String toFixed(double value, int digits) {
+        return String.format("%." + digits + "f", value);
+    }
+}
+
